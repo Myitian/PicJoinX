fb1ce2ecff46e202ec43aa64f9e3894ba2722a7e
Myitian <miaoyitian233@gmail.com>
2023-11-11 18:03:48 +0800

    original

---
+++ PicJoin/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PicJoin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_preview = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.textBox_sizeY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_sizeX = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_clr = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_preview);
+            this.groupBox1.Controls.Add(this.btn_save);
+            this.groupBox1.Controls.Add(this.textBox_sizeY);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_sizeX);
+            this.groupBox1.Location = new System.Drawing.Point(686, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 172);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "参数设置";
+            // 
+            // btn_preview
+            // 
+            this.btn_preview.Location = new System.Drawing.Point(21, 131);
+            this.btn_preview.Name = "btn_preview";
+            this.btn_preview.Size = new System.Drawing.Size(75, 23);
+            this.btn_preview.TabIndex = 5;
+            this.btn_preview.Text = "预览";
+            this.btn_preview.UseVisualStyleBackColor = true;
+            this.btn_preview.Click += new System.EventHandler(this.btn_preview_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(111, 131);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "保存";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // textBox_sizeY
+            // 
+            this.textBox_sizeY.Location = new System.Drawing.Point(144, 52);
+            this.textBox_sizeY.Name = "textBox_sizeY";
+            this.textBox_sizeY.Size = new System.Drawing.Size(42, 21);
+            this.textBox_sizeY.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "画布尺寸：";
+            // 
+            // textBox_sizeX
+            // 
+            this.textBox_sizeX.Location = new System.Drawing.Point(78, 52);
+            this.textBox_sizeX.MaxLength = 4;
+            this.textBox_sizeX.Name = "textBox_sizeX";
+            this.textBox_sizeX.Size = new System.Drawing.Size(42, 21);
+            this.textBox_sizeX.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_clr);
+            this.groupBox2.Controls.Add(this.btn_del);
+            this.groupBox2.Controls.Add(this.btn_add);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 386);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "原始图片";
+            // 
+            // btn_clr
+            // 
+            this.btn_clr.Location = new System.Drawing.Point(137, 357);
+            this.btn_clr.Name = "btn_clr";
+            this.btn_clr.Size = new System.Drawing.Size(41, 23);
+            this.btn_clr.TabIndex = 3;
+            this.btn_clr.Text = "清空";
+            this.btn_clr.UseVisualStyleBackColor = true;
+            this.btn_clr.Click += new System.EventHandler(this.btn_clr_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(69, 357);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(41, 23);
+            this.btn_del.TabIndex = 2;
+            this.btn_del.Text = "删除";
+            this.btn_del.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(7, 357);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(41, 23);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "添加";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView1.Location = new System.Drawing.Point(7, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(171, 319);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Location = new System.Drawing.Point(202, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(478, 386);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "拼接预览";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(466, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 404);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_sizeY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_sizeX;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_clr;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btn_preview;
+    }
+}
+
---
+++ PicJoin/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PicJoin
+{
+    public partial class Form1 : Form
+    {
+        int pic_sizeX, pic_sizeY;
+        ImageList imageList;
+        Image imageResult = null;
+        List<Image> picList = new List<Image>();
+
+        /// <summary>
+        /// 获取画布大小
+        /// </summary>
+        private void GetPicSize()
+        {
+            try
+            {
+                pic_sizeX = Convert.ToInt16(textBox_sizeX.Text);
+                pic_sizeY = Convert.ToInt16(textBox_sizeY.Text);
+            }
+            catch (System.Exception ex)
+            {
+                textBox_sizeX.Text = pic_sizeX.ToString();
+                textBox_sizeY.Text = pic_sizeY.ToString();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pic_sizeX = 512;
+            pic_sizeY = 512;
+            imageList = new ImageList();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_sizeX.Text = "512";
+            textBox_sizeY.Text = "512";
+
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "图片文件(*.bmp,*.jpg,*.png,*.gif）|*.bmp;*.jpg;*.png;*.gif|所有文件(*.*)|*.*";
+            openFileDialog1.Title = "选择图片";
+            openFileDialog1.Multiselect = true;
+            string picNames = string.Empty;
+            //imageList.ImageSize = new Size(64, 64);
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listView1.LargeImageList = imageList;
+                foreach (string fileName in openFileDialog1.FileNames)
+                {
+                    picNames += fileName + Environment.NewLine;
+                    Image image = Image.FromFile(fileName);
+                    Size isize = image.Size;
+                    imageList.ImageSize = new Size(64,64);
+                    imageList.Images.Add(image);
+                    picList.Add(image);
+                    ListViewItem myitem = new ListViewItem(Path.GetFileName(fileName));
+                    myitem.ImageIndex = imageList.Images.Count - 1;
+                    listView1.Items.Add(myitem);
+
+
+
+                }
+
+                // MessageBox.Show(picNames);
+            }
+        }
+
+        private void btn_clr_Click(object sender, EventArgs e)
+        {
+            imageList.Images.Clear();
+            listView1.Items.Clear();
+            picList.Clear();
+        }
+
+        private void btn_preview_Click(object sender, EventArgs e)
+        {
+            GetPicSize();
+            //pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Size = new Size(pic_sizeX, pic_sizeY);
+            imageResult = JoinImage(picList, pic_sizeX, pic_sizeY);
+            pictureBox1.Image = imageResult;
+
+        }
+
+
+        private Image JoinImage(List<Image> imageList, int maxX, int maxY)
+        {
+            //图片列表
+            if (imageList.Count <= 0)
+                return null;
+            //构造最终的图片白板
+            Bitmap tableChartImage = new Bitmap(maxX, maxY);
+            Graphics graph = Graphics.FromImage(tableChartImage);
+            //初始化这个大图
+            graph.DrawImage(tableChartImage, 0, 0);
+            //初始化当前宽
+            int currentWidth = 0;
+            int currentHeight = 0;
+            int maxHeightLine = 0;//一行中最大高度
+            foreach (Image i in imageList)
+            {
+                if (maxHeightLine < i.Height)
+                {
+                    maxHeightLine = i.Height;
+                }
+                if (currentWidth + i.Width > maxX)
+                {
+                    currentWidth = 0;
+                    currentHeight += maxHeightLine;
+                    if (currentHeight >= maxY)
+                    {
+                        break;
+                    }
+                }
+                if (currentHeight + maxHeightLine >= maxY)
+                {
+                    break;
+                }
+
+                //拼图
+                graph.DrawImage(i, currentWidth, currentHeight);
+                //拼接改图后，当前宽度
+                currentWidth += i.Width;
+
+            }
+            return tableChartImage;
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            btn_preview_Click(sender, e);
+            saveFileDialog1.Filter = "PNG图片(*.png)|*.png";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.DefaultExt = "png";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imageResult.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+    }
+}
+
